e6e59f2ac1f18182036ace72b66123fa98956df9
BenitoCamelo2 <61254508+BenitoCamelo2@users.noreply.github.com>
2022-03-18 00:04:49 -0600

    Added MainMenu.cpp and MainMenu.h

---
+++ MainMenu.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Benito on 18/03/2022.
+//
+
+#include "MainMenu.h"
---
+++ MainMenu.h
@@ -0,0 +1,10 @@
+#ifndef PROGRAMA_MAINMENU_H
+#define PROGRAMA_MAINMENU_H
+
+
+class MainMenu {
+
+};
+
+
+#endif //PROGRAMA_MAINMENU_H
